0d36c8d1b3704d7bc1f723cee5c4a7f958e844c7
lsj333610dl <lsj333610dl@naver.com>
2014-07-10 16:19:49 +0900

    array 출력

--- week1/fileAndString/fileAndString/main.m
+++ week1/fileAndString/fileAndString/main.m
@@ -8,15 +8,48 @@
 
 #import <Foundation/Foundation.h>
 
+//void printAllFiles(){
+//    //파일 매니저 생성
+//    NSFileManager *fileManager = [NSFileManager defaultManager];
+//    
+//    
+//    //디렉토리
+//    NSURL *directoryURL = [fileManager URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+//    
+//    NSEnumerator *enumerator = [fileManager enumeratorAtURL:directoryURL includingPropertiesForKeys:@[NSURLNameKey, NSURLIsDirectoryKey, NSURLContentModificationDateKey] options:NSDirectoryEnumerationSkipsHiddenFiles errorHandler:nil];
+//    
+//    NSLog(@"%@",[enumerator allObjects]);
+//}
+
+NSArray* getAllfilesAtPath(NSString* dirPath){
+    NSFileManager * fileManager = [NSFileManager defaultManager];
+    
+    
+    NSArray *arrayFiles = [fileManager contentsOfDirectoryAtPath:dirPath error:nil];
+    
+    NSMutableArray *resultArray = [[NSMutableArray alloc]initWithArray:arrayFiles];
+    
+    for (int i=0; i<[arrayFiles count]; i++) {
+        if ([arrayFiles[i] rangeOfString:@"."].location == NSNotFound) {
+            NSLog(@"디렉토리");
+            NSString *deepPath = [NSString stringWithFormat:@"%@/%@",dirPath,arrayFiles[i]];
+            
+            NSArray *arrayDeepDirFiles = [fileManager contentsOfDirectoryAtPath:deepPath error:nil];
+            [resultArray addObjectsFromArray:arrayDeepDirFiles];
+        }
+    }
+    
+    return resultArray;
+}
+
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        //모든 파일 출력
+        NSLog(@"%@",getAllfilesAtPath(@"/Users/retina15/Dropbox/wordpress/videostar"));
     }
+    
     return 0;
 }
-
