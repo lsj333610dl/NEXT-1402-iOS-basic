d862d6a1e3daeb10716620c614468e826f9fe05c
lsj333610dl <lsj333610dl@naver.com>
2014-07-17 15:41:55 +0900

    Initial Commit

---
+++ week2/JSON/JSON/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JSON
+//
+//  Created by 이상진 on 2014. 7. 17..
+//  Copyright (c) 2014년 EntusApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
