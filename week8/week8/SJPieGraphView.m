81384c0c6a634a76350c3d9ad42a909c61eed169
lsj333610dl <lsj333610dl@naver.com>
2014-08-28 18:10:08 +0900

    pie 완료

--- week8/week8/SJPieGraphView.m
+++ week8/week8/SJPieGraphView.m
@@ -10,55 +10,82 @@
 #import "SJPieGraphView.h"
 
 #define radian(degree) degree*M_PI/180
+#define DEGREE_BY_PERCENTAGE 360.0f*([[datas[i-1] objectForKey:@"percentage"] floatValue]/100.0f)
+#define DEGREE_BY_PERCENTAGE2 360.0f*([[datas[i] objectForKey:@"percentage"] floatValue]/100.0f)
+
 
 NSArray *datas;
+NSMutableArray *degrees;
+double total;
 
 @implementation SJPieGraphView
 
 
 - (void)initWithArray:(NSArray*)array{
     datas = array;
+    degrees = [[NSMutableArray alloc]initWithCapacity:[datas count]];
+    
+    for (NSDictionary *data in datas) {
+        total += [[data objectForKey:@"percentage"] floatValue];
+    }
+    
+    [self getDegrees];
 }
 
+- (void)getDegrees{
+    for (NSDictionary *data in datas) {
+        NSNumber *degree = [NSNumber numberWithFloat:
+                            360.0f*([[data objectForKey:@"percentage"] floatValue]/total)];
+        [degrees addObject:degree];
+    }
+}
 
 - (void)drawRect:(CGRect)rect{
 
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGPoint center = CGPointMake(rect.size.width/2, rect.size.height/2);
     
     
-    CGContextBeginPath(context);
-    CGContextMoveToPoint(context, center.x, center.y);
     
-    CGContextSaveGState(context);
-    
-    CGContextSetFillColorWithColor(context, [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1].CGColor);
-    
-    CGContextAddArc(context, center.x, center.y, center.x, radian(-(360*([[datas[0] objectForKey:@"percentage"] floatValue]/100))), radian(-(360*([[datas[1] objectForKey:@"percentage"] floatValue]/100))), YES);
-    
-    CGContextDrawPath(context, kCGPathFill);
-    
-//    for (int i=1; i<[datas count]-1; i++) {
+//    CGContextRef context = UIGraphicsGetCurrentContext();
+//    CGPoint center = CGPointMake(rect.size.width/2, rect.size.height/2);
 //
-//        NSLog(@"%lf",360*([[datas[i] objectForKey:@"percentage"] floatValue]/100) );
-//        
-//        CGContextRef context = UIGraphicsGetCurrentContext();
-//        CGPoint center = CGPointMake(rect.size.width/2, rect.size.height/2);
-//        
-//        
-//        CGContextBeginPath(context);
-//        CGContextMoveToPoint(context, center.x, center.y);
-//        
-//        CGContextSaveGState(context);
-//        
-//        CGContextSetFillColorWithColor(context, [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1].CGColor);
-//        
-//        CGContextAddArc(context, center.x, center.y, center.x, radian(-(360*([[datas[i] objectForKey:@"percentage"] floatValue]/100))), radian(-(360*([[datas[i+1] objectForKey:@"percentage"] floatValue]/100))), YES);
-//        
-//        CGContextDrawPath(context, kCGPathFill);
-//    }
+//    
+//    CGContextBeginPath(context);
+//    CGContextMoveToPoint(context, center.x, center.y);
+//
+//    CGContextSaveGState(context);
+//    
+//    CGContextSetFillColorWithColor(context, [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1].CGColor);
+//    
+//    CGContextAddArc(context, center.x, center.y, center.x, radian((360*([[datas[3] objectForKey:@"percentage"] floatValue]/100))), radian((360*([[datas[2] objectForKey:@"percentage"] floatValue]/100))), NO);
+//    
+//    
+//    CGContextDrawPath(context, kCGPathFill);
     
+    double lastDegree = 0.0;
+    
+    for (int i=1; i<[degrees count]; i++) {
 
+        
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGPoint center = CGPointMake(rect.size.width/2, rect.size.height/2);
+        
+        
+        CGContextBeginPath(context);
+        CGContextMoveToPoint(context, center.x, center.y);
+        
+        CGContextSaveGState(context);
+        
+        CGContextSetFillColorWithColor(context, [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1].CGColor);
+        
+        
+        lastDegree += [degrees[i-1] floatValue];
+        
+        CGContextAddArc(context, center.x, center.y, center.x, radian(lastDegree), radian([degrees[i] floatValue]), NO);
+        
+        CGContextDrawPath(context, kCGPathFill);
+    }
+    
+//    NSLog(@"%lf",total);
 //    for (int i=0; i<[datas count]; i++) {
 //        CGContextRef context = UIGraphicsGetCurrentContext();
 //        CGPoint center = CGPointMake(rect.size.width/2, rect.size.height/2);
