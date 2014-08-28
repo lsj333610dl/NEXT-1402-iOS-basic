27220e1255330aca212c8d985a6087edd4444800
lsj333610dl <lsj333610dl@naver.com>
2014-08-28 21:01:33 +0900

    pieGraph에 라벨 추가.

--- week8/week8/SJPieGraphView.m
+++ week8/week8/SJPieGraphView.m
@@ -10,9 +10,6 @@
 #import "SJPieGraphView.h"
 
 #define radian(degree) degree*M_PI/180
-#define DEGREE_BY_PERCENTAGE 360.0f*([[datas[i-1] objectForKey:@"percentage"] floatValue]/100.0f)
-#define DEGREE_BY_PERCENTAGE2 360.0f*([[datas[i] objectForKey:@"percentage"] floatValue]/100.0f)
-
 
 NSArray *datas;
 NSMutableArray *degrees;
@@ -44,9 +41,8 @@ double total;
 
     double lastDegree = 0;
     
-    
+    //그래프
     for (int i=0; i<[degrees count]; i++) {
-
         
         CGContextRef context = UIGraphicsGetCurrentContext();
         CGPoint center = CGPointMake(rect.size.width/2, rect.size.height/2);
@@ -58,7 +54,6 @@ double total;
         CGContextSaveGState(context);
         
         CGContextSetFillColorWithColor(context, [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1].CGColor);
-        
         
         if (i==0) {
             lastDegree = [[degrees lastObject]floatValue];
@@ -75,11 +70,49 @@ double total;
             CGContextAddArc(context, center.x, center.y, center.x, radian(lastDegree), radian(([degrees[i] floatValue]+lastDegree)), NO);
         }
         
-        
-        
-        
         CGContextDrawPath(context, kCGPathFill);
     }
+    
+
+    //라벨
+    for (int i=0; i<[degrees count]; i++) {
+        
+        CGPoint center = CGPointMake(rect.size.width/2, rect.size.height/2);
+        
+        
+        
+        UILabel *label = [UILabel new];
+        [label setTextAlignment:NSTextAlignmentCenter];
+        
+        [label setText:[datas[i] objectForKey:@"title"]];
+        [label setFrame:CGRectMake(0, 0, center.x, 40)];
+        [label setCenter:center];
+        [label setTextColor:[UIColor whiteColor]];
+        [label.layer setAnchorPoint:CGPointMake(0, 0.5)];
+        
+        [self addSubview:label];
+        
+        if (i==0) {
+            lastDegree = [[degrees lastObject]floatValue];
+            label.transform = CGAffineTransformMakeRotation(  ( radian(lastDegree)+radian(([degrees[i] floatValue]+lastDegree)) )/2  );
+            
+        }
+        else if (i==[degrees count]-1) {
+            
+            label.transform = CGAffineTransformMakeRotation(  ( radian(0)+radian(([[degrees lastObject]floatValue])) )/2  );
+        }
+        else {
+            
+            lastDegree += [degrees[i-1] floatValue];
+            
+            label.transform = CGAffineTransformMakeRotation(  ( radian(lastDegree)+radian(([degrees[i] floatValue]+lastDegree)) )/2  );
+            
+        }
+        
+    }
+    
+    
+    
     
 }
 
